5688121768f18a1a8c319b453dba14d48d56c20e
DiegoRendon2001 <103530306+DiegoRendon2001@users.noreply.github.com>
2022-11-18 15:21:33 -0800

    Add files via upload

---
+++ GlobalWarmingIndivProj/js/script.js
@@ -0,0 +1,12 @@
+const quoteText = document.getElementById('quote-text'),
+      quoteTags = document.getElementById('quote-tags'),
+      quoteAuthor = document.getElementById('quote-author'),
+      genQuoteBtn = document.getElementById('gen-quote-btn');
+
+fetch('https://api.quotable.io/random')
+    .then(response => response.json())
+    .then(data => {
+        quoteText.textContent = data.content;
+        quoteTags.textContent = data.tags;
+        quoteAuthor.textContent = `-- ${data.author}`;
+    })
